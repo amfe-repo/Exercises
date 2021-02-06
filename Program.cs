2657a21902d4331c81d3823323db7154e82f24b3
amfe-repo <202010603@itla.edu.do>
2021-02-06 12:34:33 -0400

    Fix error

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Exercises
             //obj.ListOfNumbers(); //Programa #1
             //obj.ConvertionsMenu(); //Programa #2
             //obj.MultiplicationTable(); //Programa #3
-            obj.CalculateSalaryEmploye();
+            //obj.CalculateSalaryEmploye();
         }
     }
 }
